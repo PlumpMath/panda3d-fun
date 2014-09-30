94be79f6dc401309b825f8727eeddb3873eb0359
atatsu <nathan.lundquist@gmail.com>
2014-09-29 20:57:37 -0500

    added a second cube

--- network/client/client.py
+++ network/client/client.py
@@ -9,7 +9,7 @@ class Client(ShowBase):
         # self.camera.setPos(0, 0, 0)
         # self.camera.setHpr(10, 0, 0)
 
-        self.cube = self.loader.loadModel('models/cube')
+        self.cube = self.loader.loadModel('models/cube01')
         self.cube.reparentTo(self.render)
         self.cube.setPos(0, 15, -1)
         self.cube.setHpr(20, 10, 0)
